2f1b128b0d7dfb9ed63de38e8eab6497b1ddb27f
raman <raman@gmail.com>
2022-08-14 15:06:24 +0000

    AppTest.java

--- src/test/java/com/raman/AppTest.java
+++ src/test/java/com/raman/AppTest.java
@@ -9,12 +9,8 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
+    public static void main( string[] args )
     {
-        assertTrue( true );
+    	system.out.println( "changes are done by raman" );
     }
 }
